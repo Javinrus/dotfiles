4c6e641a54f1d89d57b29298f63b0ac84cf88f5a
Javin Ruskyll <javelinwolfram51@proton.me>
2026-05-30 22:39:05 +0700

    Update environment.lua

--- .config/hypr/parts/environment.lua
+++ .config/hypr/parts/environment.lua
@@ -1,18 +1,18 @@
 -- Environment variables
 local HOME = os.getenv("HOME")
 
--- Wayland variables
-hl.env("GDK_BACKEND", "wayland")
-hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
-
--- Qt environment variables
-hl.env("QT_QPA_PLATFORM", "wayland")
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
-
--- Cursors
+-- Cursor variables
 hl.env("XCURSOR_SIZE", "32")
 hl.env("XCURSOR_THEME", "Nordzy-cursors")
 hl.env("HYPRCURSOR_SIZE", "32")
 hl.env("HYPRCURSOR_THEME", "Nordzy-hyprcursors")
+
+-- Wayland variables
+hl.env("GDK_BACKEND", "wayland")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+
+-- Qt related variables
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
