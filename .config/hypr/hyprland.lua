d98878975ebb68137a42c2abafadf0bf7777a1a4
Javin Ruskyll <javelinwolfram51@proton.me>
2026-05-30 21:25:27 +0700

    Update hyprland.lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,5 +1,5 @@
 -- Split the config to organize better
 require("parts.monitors")
-require("parts.env")
-require("parts.startup")
-require("parts.binds")
+require("parts.autostart")
+require("parts.environment")
+require("parts.keybinds")
