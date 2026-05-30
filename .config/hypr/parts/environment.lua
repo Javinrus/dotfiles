9792e45a89e42274cf7090c0a0b304e73d00874d
Javin Ruskyll <javelinwolfram51@proton.me>
2026-05-30 22:39:39 +0700

    Update environment.lua

--- .config/hypr/parts/environment.lua
+++ .config/hypr/parts/environment.lua
@@ -16,3 +16,6 @@ hl.env("QT_QPA_PLATFORM", "wayland")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+
+-- To be continued
+-- File directories be streamlined
