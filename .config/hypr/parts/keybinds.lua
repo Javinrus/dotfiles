40ff0cea1a6afb4a540c770e90dd8392a63b8cfb
Javin Ruskyll <javelinwolfram51@proton.me>
2026-05-31 00:21:38 +0700

    Update keybinds.lua

--- .config/hypr/parts/keybinds.lua
+++ .config/hypr/parts/keybinds.lua
@@ -2,10 +2,10 @@
 local scripts = "~/.local/bin/"
 
 local terminal = "foot"
+local menu     = "rofi"
 local files    = "thunar"
 local code     = "vscodium"
 local browser  = "brave-origin-nightly"
-local launcher = "rofi"
 
 -- Writing hl.dsp.exec_cmd() all the time is too long
 local function run(cmd, rules)
@@ -20,8 +20,11 @@ end
 hl.bind("SUPER + Q",          run(terminal))
 hl.bind("SUPER + SHIFT + Q"), run(terminal, {float = true}))
 
--- Launch menus
-hl.bind("SUPER + R", run(launcher .. " -show drun"))
-hl.bind("SUPER + W", run(launcher .. " -show window"))
+-- Launch some menu
+hl.bind("SUPER + E",      run("rofimoji"))
+hl.bind("SUPER + R",      run(menu .. " -show drun"))
+hl.bind("SUPER + W",      run(menu .. " -show window"))
+hl.bind("SUPER + Z",      slash("wallpaper-select.sh"))
+hl.bind("SUPER + ESCAPE", run("wlogout"))
 
 -- Launch some apps
