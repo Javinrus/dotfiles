ab6a7babf8de6be13cfb2a9e13c8dec4bbc445b0
Javin Ruskyll <javelinwolfram51@proton.me>
2026-05-31 20:36:31 +0700

    Update autostart.lua

--- .config/hypr/parts/autostart.lua
+++ .config/hypr/parts/autostart.lua
@@ -3,7 +3,7 @@ local autostart = {
   "wayle panel start", -- Status bar
   "swaybg", -- Wallpaper daemon
   "swaync", -- Notification daemon
-  "wl-clip-persist --clipboard regular" -- Enable clipboard persistence
+  "wl-clip-persist --clipboard regular", -- Enable clipboard persistence
   "/usr/lib/xfce-polkit/xfce-polkit", -- Policy manager (prompts permission if app requests)
   "bash -c '[[ -f \"$HOME/.current_wallpaper\" ]] && swaybg -i \"$(cat $HOME/.current_wallpaper)\" -m fill'" -- Restore wallpaper
 }
