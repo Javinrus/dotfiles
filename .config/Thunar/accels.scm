fd21348ce891f28eac3f0bee0dc93f0bcb1d3d92
Javin Ruskyll <javelinwolfram51@proton.me>
2026-06-19 15:50:48 +0700

    Update accels.scm

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,1 +1,3 @@
+; This was modified as of version 4.20
 (gtk_accel_path "<Actions>/ThunarWindow/open-parent" "BackSpace")
+(gtk_accel_path "<Actions>/ThunarActionManager/open-with-other" "<Shift>Return")
