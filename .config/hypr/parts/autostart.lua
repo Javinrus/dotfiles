876f416b9f8a82e92b331697c71064b973c629d0
Javin Ruskyll <javelinwolfram51@proton.me>
2026-06-27 23:02:07 +0700

    Fix wallpaper restoration

--- .config/hypr/parts/autostart.lua
+++ .config/hypr/parts/autostart.lua
@@ -2,15 +2,15 @@
 local autostart = {
   "wayle panel start", -- Status bar
   "awww-daemon", -- Wallpaper daemon
+  "awww restore", -- Restore cached wallpaper
   "mako", -- Notification daemon
   "wl-clip-persist --clipboard regular", -- Enable clipboard persistence
   "/usr/lib/xfce-polkit/xfce-polkit", -- Policy manager
   "elephant", -- Data provider service and backend
-  "walker --gapplication-service", -- MultiPurpose launcher, run it as a service for faster startup speed
-  "bash -c '[[ -f \"$HOME/.current_wallpaper\" ]] && swaybg -i \"$(cat $HOME/.current_wallpaper)\" -m fill'" -- Restore wallpaper
+  "walker --gapplication-service" -- MultiPurpose launcher, run it as a service for faster startup speed
 }
 
--- Then autostart the listed programs
+-- Then autostart all listed programs
 hl.on("hyprland.start", function()
     for i = 1, #autostart do
         hl.exec_cmd(autostart[i])
