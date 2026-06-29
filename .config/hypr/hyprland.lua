35bc886a465d13a508fab5398ee3ee9373d1a5a9
Javin Ruskyll <javelinwolfram51@proton.me>
2026-06-29 20:49:59 +0700

    Add all parts into list

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,10 +1,14 @@
--- This was modified as of version 0.55.2
--- This config is a multifile, splitting helps to organize better
+-- This was modified as of version 0.55.4
 local parts = {
     "parts.monitors",
     "parts.environment",
     "parts.autostart",
     "parts.keybinds",
+    "parts.gestures",
+    "parts.rules",
+    "parts.main",
+    "parts.animations",
+    "parts.theme"
 }
 
 for _, i in ipairs(parts) do
