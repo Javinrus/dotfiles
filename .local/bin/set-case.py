e5a02ef839f261654c0f990281bfef66bcabb58d
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-08-20 22:40:03 +0700

    Update set-case.py

--- .local/bin/set-case.py
+++ .local/bin/set-case.py
@@ -5,3 +5,13 @@ import re
 import sys
 import subprocess
 from pathlib import Path
+
+CASE = sys.argv[1] if len(sys.argv) > 1 else ""
+FILES = [Path(x) for x in sys.argv[2:]]
+
+VALID_CASES = {
+    "camel",
+    "snake",
+    "kebab",
+    "spaced",
+}
