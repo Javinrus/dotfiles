bdef428c9f3fffaca1bbdf5e507a0fd9135587ce
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-08-25 22:25:28 +0700

    Update env.lua

--- .config/hypr/parts/env.lua
+++ .config/hypr/parts/env.lua
@@ -1,10 +1,12 @@
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+local HOME = os.getenv("HOME")
 
 -- Themes
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
+-- I'm making my own cursor theme soon
 -- Using Nordzy's for now
 hl.env("HYPRCURSOR_THEME", "Nordzy-hyprcursor")
 hl.env("HYPRCURSOR_SIZE", "32")
@@ -22,3 +24,19 @@ hl.env("CLUTTER_BACKEND", "wayland")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Good sir, what is this?
+-- Basically helps apps to figure out where to store:
+local XDG_STATE_HOME  = HOME .. "/.local/state" -- Persistent state/history
+local XDG_DATA_HOME   = HOME .. "/.local/share" -- Persistent application data
+local XDG_CONFIG_HOME = HOME .. "/.config"      -- Configuration files
+local XDG_CACHE_HOME  = HOME .. "/.cache"       -- Disposable cache
+
+-- Global XDG environment variables 
+hl.env("XDG_STATE_HOME",  XDG_STATE_HOME)
+hl.env("XDG_DATA_HOME",   XDG_DATA_HOME)
+hl.env("XDG_CONFIG_HOME", XDG_CONFIG_HOME)
+hl.env("XDG_CACHE_HOME",  XDG_CACHE_HOME)
+
+-- I found this very useful, thanks man!
+-- https://github.com/cebem1nt/dotfiles/blob/main/.config/hypr/config/env.lua
