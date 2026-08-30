fceff350e11e35665297bd322980bb2571cca470
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-08-30 22:18:45 +0700

    Some big changes

--- .config/hypr/parts/env.lua
+++ .config/hypr/parts/env.lua
@@ -41,9 +41,34 @@ hl.env("XDG_DATA_HOME",   XDG_DATA_HOME)
 hl.env("XDG_CONFIG_HOME", XDG_CONFIG_HOME)
 hl.env("XDG_CACHE_HOME",  XDG_CACHE_HOME)
 
--- Force some apps to use XDG base directories
-hl.env("CARGO_HOME",    XDG_DATA_HOME   .. "/rust-cargo")
-hl.env("DOCKER_CONFIG", XDG_CONFIG_HOME .. "/docker")
+--
+hl.env("XDG_TEMPLATES_DIR",   XDG_DATA_HOME .. "/templates") -- ~/Templates to ~/.local/share/templates
 
--- Thanks, user from the internet!
+-- Force some apps to use XDG base directories
+hl.env("HISTFILE",               XDG_STATE_HOME  .. "/bash_history")
+hl.env("PYTHON_HISTORY",         XDG_STATE_HOME  .. "/python_history")
+hl.env("NPM_CONFIG_USERCONFIG",  XDG_CONFIG_HOME .. "/npm/npmrc")
+hl.env("NPM_CONFIG_CACHE",       XDG_CACHE_HOME  .. "/npm")
+hl.env("RUSTUP_HOME",            XDG_DATA_HOME   .. "/rustup")
+hl.env("CARGO_HOME",             XDG_DATA_HOME   .. "/cargo")
+hl.env("DOCKER_CONFIG",          XDG_CONFIG_HOME .. "/docker")
+hl.env("ANDROID_USER_HOME",      XDG_CONFIG_HOME .. "/android")
+hl.env("GOPATH",                 XDG_DATA_HOME   .. "/go")
+hl.env("GOMODCACHE",             XDG_CACHE_HOME  .. "/go/mod")
+
+hl.env(
+    "_JAVA_OPTIONS",
+    "-Djava.util.prefs.userRoot=" .. XDG_CONFIG_HOME .. "/java"
+)
+
+hl.env(
+    "GTK2_RC_FILES",
+    string.format(
+        "%s/gtk-2.0/gtkrc:%s/gtk-2.0/gtkrc.mine",
+        XDG_CONFIG_HOME,
+        XDG_CONFIG_HOME
+    )
+)
+
+-- That part above is based from this:
 -- https://github.com/cebem1nt/dotfiles/blob/main/.config/hypr/config/env.lua
