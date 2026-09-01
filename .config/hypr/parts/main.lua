1e579a3343096c75d89cfbc565ff92ddb4c089da
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-09-01 10:07:13 +0700

    Initial

--- .config/hypr/parts/main.lua
+++ .config/hypr/parts/main.lua
@@ -1,1 +1,23 @@
 --
+hl.config({
+    misc = {
+        animate_manual_resizes       = false,
+        animate_mouse_windowdragging = false,
+
+        disable_hyprland_logo        = true,
+        force_default_wallpaper      = 0,
+
+        on_focus_under_fullscreen    = 2,
+        allow_session_lock_restore   = true,
+        middle_click_paste           = false,
+        session_lock_xray            = false,
+
+
+        disable_splash_rendering     = true,
+        background_color             = "rgb(" .. theme.surfaceContainer .. ")", -- To be changed
+    },
+
+    debug = {
+        error_position = 1
+    }
+})
